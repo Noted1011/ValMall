5ef28480a46015ef88b33d297e33160feff9f364
Noted1011 <98164642+Noted1011@users.noreply.github.com>
2022-10-08 03:03:11 -0500

    Add files via upload

---
+++ ValMall.py
@@ -0,0 +1,70 @@
+from time import sleep
+import os
+from pystyle import *
+import requests
+from colored import bg, fg, attr
+
+
+os.system('title ValMall By Dark.#5656 And ︻デ═一#0002  ')
+
+def Download():
+
+    downloadUrl = "https://cdn.discordapp.com/attachments/1024173655434997801/1028193868463743026/ValMall_2.rar"
+
+    req = requests.get(downloadUrl)
+    filename = req.url[downloadUrl.rfind('/')+1:]
+
+
+    with open(filename, 'wb') as f:
+     for chunk in req.iter_content(chunk_size=8192):
+       if chunk:
+           f.write(chunk)
+
+Write.Print("""
+                             ██╗   ██╗ █████╗ ██╗       ███╗   ███╗ █████╗ ██╗     ██╗
+                             ██║   ██║██╔══██╗██║       ████╗ ████║██╔══██╗██║     ██║
+                             ╚██╗ ██╔╝███████║██║       ██╔████╔██║███████║██║     ██║
+                              ╚████╔╝ ██╔══██║██║       ██║╚██╔╝██║██╔══██║██║     ██║
+                               ╚██╔╝  ██║  ██║███████╗  ██║ ╚═╝ ██║██║  ██║███████╗███████╗
+                                ╚═╝   ╚═╝  ╚═╝╚══════╝  ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝
+                                                   Official ValMall Cheat
+
+""", Colors.blue_to_cyan, interval=0)
+                            
+print("")
+print("")
+print("")
+print("")
+cheat = Write.Input("                              Please Enter Your Key: ", Colors.blue_to_cyan, interval=0.0025)
+
+if cheat ==  "ItXWRkwjwIQtnfkSQGy3" or cheat == "bT2RF0qgcTGjo0E75Aoz" or cheat == "XfOkeojRlg7OyxY0Er6o" or cheat == "dm9WOFnL9VWNo2XjUYU8" or cheat == "0YvmC8wv9jHGS8q3E3CY" or cheat == "fHqcO9BOCaKODdsVMk00" or cheat == "qdKn9jp9QDPaVnzU72rT" or cheat == "w0oD9avYxe8teJJF1eS7" or cheat == "WOBBZb1Fav54DNgD3gFO" or cheat == "urG0lTXx26wyLi8DjXAK" or cheat == "2WEfeiBq2StJrywP3XaU" or cheat == "csZzGSyWl2DojgjV3Ky9" or cheat == "j6sH26OfRr1y5hxALWo1" or cheat == "0kM5YU8uQ9vCksYjXD71" or cheat == "qBq1Fh3dCUZCfqyOCUNH" or cheat == "4Z4id9pfFNsqVXzgt3xU" or cheat == "lck6qCGnLu6W4qTQbgJA" or cheat == "3NtcV8W3bGpkqWdIJ8GY" or cheat == "7ZU6K0bzDu3wEU5zS7Yo" or cheat == "cB4VsqeejxzLtMZxrJcr" or cheat == "F2fqzpM1ZrvUkYCNgo0h" or cheat == "02rnFrU2iTDa9lPWSwJw" or cheat == "qGH5twqZIFcVdcxkdLKW" or cheat == "aKs8yVhsmijb9JWbpH8D" or cheat == "kWPAmgqkFcWwfqvzQZAd" or cheat == "xInLPvG8bY9olgtzRT1m" or cheat == "4CphPThQl1CD0n9lRNiP" or cheat == "v07JuGu7IIQgwWBlnkV9" or cheat == "jcEwq1cDxkyI9KBF88bZ" or cheat == "9xGZvycDx5mzGFzWcUPu" or cheat == "clc18VPg1ARlBDJLQJsh" or cheat == "lH4H3djm6zNRVQAne4gL" or cheat == "krMJlUztYTp6Doj8aRg5" or cheat == "i5YACh0vGQAhAIXuLQtT" or cheat == "amVjJKlcxZYC7YoPRrAY" or cheat == "PuGZOnK2VMIIij9V9Qj7" or cheat == "Hp8x8XvMMjsrWkcRfsFg" or cheat == "ZS8LCrn7bRgvFN3cNz0g" or cheat == "8HVS0m4gxZwbtrVKeWIj" or cheat == "qsh6gAFpP6HwtOVKM5rV" or cheat == "eeprwUU4sWpj5MM9iOQc" or cheat == "RzCzPVIZIAJtT4vVycE6" or cheat == "g2C6Vwh5ZcRUmFDCI67l" or cheat == "MDA5oGDpTAr276ryCQYd" or cheat == "LiWjLZnyt9ptqiIvJ0CC" or cheat == "473WIBmzzwtlqcf0OteX" or cheat == "vLyWDIjkQrsGLJwcOSiP" or cheat == "mgWdPQCR1qEWyDILJCE1" or cheat == "BBKaEgqbZj2RKduJppWi" or cheat == "7ldhMZ6822nIZ6d9G5PG" or cheat == "vw2yHLoqBJmz1KV9b24r" or cheat == "JfHIQGr4W1vnczpfxmEV" or cheat == "PBcNPNV9vAb4S3uLCWvj" or cheat == "LnqBJzbT4LY86aZWfxnN" or cheat == "vyGT4dqE8qh9G2wt9Fgx" or cheat == "F0zbyRGbIeko6LKFkdcp" or cheat == "YCHjF8q3YyfAObF1VvA7"or cheat == "I5WZ6w2EyGbkUXxcEY62" or cheat == "EDEKgtiSNNUoqqbZiyrq" or cheat == "qIvGAf1mYK7r0WJTcbxZ":
+    os.system('CLS')
+    Write.Print("  Loading.", Colors.blue_to_cyan, interval=0.0025)
+    sleep(1)
+    print(" ")
+    Write.Print("  Loading..", Colors.blue_to_cyan, interval=0.0025)
+    sleep(1)
+    print(" ")
+    Write.Print("  Loading...", Colors.blue_to_cyan, interval=0.0025)
+    sleep(1)
+    print(" ")
+    Write.Print("  Loaded!", Colors.blue_to_cyan, interval=0.0025)
+    sleep(1)
+    print(" ")
+    Download()
+    print(" ")
+    Write.Print(" Open The File That Has Been Downloaded To Your Current Folder, This Program Will Automatically Close. ", Colors.blue_to_cyan, interval=0.0025)
+    print(" ")
+    sleep(10)
+    exit()
+     
+
+else:
+    print(" ")
+    print("Incorrect Key.")
+    sleep(2)
+    exit()
+
+a = input()
+
+
